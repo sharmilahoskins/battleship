82e3062e46f6a9c06311390c1eda287ffb59118a
Amy Dyson <amy.dyson@mac.com>
2017-06-14 16:06:32 -0700

    added start button

--- epic1.js
+++ epic1.js
@@ -42,44 +42,72 @@ function makeTable() {
 }
 
 //Controller***********************************************
+//***********************************************
 
 $(document).ready(function(){
   makeTable() //create board
-  placeShips() //places ships
+  $("td").off("click");
+
+  // placeShips() //places ships
+
+  $(".btn").click(function(){
+
+    //make board all light blue:
+    $("td").removeClass("torpedoed");
+    $("td").removeClass("hit");
+    $("td").removeClass("showShip");
+
+    shipArray=[];
+    board = [[],[],[],[],[],[],[],[],[],[]];
+    hits=0;
+    torpedoesUsed=0;
+    $("#hitTracker").text("Hits: " + hits);
+    $("#torpedoTracker").text("Torpedoes used: " + torpedoesUsed);
+    $("#winLose").text("");
+
+    $(".btn").hide();
+
+    placeShips() //places ships
+
+    $("td").click(function(){
+      var currentTd = $(this).attr("id")
+      var dig0 = currentTd[0]
+      var dig1 = currentTd[1]
+      if (board[dig0][dig1] === ship){
+        $(this).addClass("hit");
+        hits ++
+        $("#hitTracker").text("Hits: " + hits)
+        if (hits === 5) {
+          $("#winLose").text("You Won!!!");
+          $("td").off("click");
+            $(".btn").show();
+        }
+      } else {
+        $(this).addClass("torpedoed");
+      }
+      $(this).off("click");
+      torpedoesUsed++;
+      $("#torpedoTracker").text("Torpedoes used: " + torpedoesUsed);
+      if(torpedoesUsed === 25){
+        $("#winLose").text("Game Over.");
+        $("td").off("click");
+        $(".btn").show();
+
+
+        for (i = 0; i < 5; i++){
+          var temp = "#" + shipArray[i]
+          if (!($(temp).hasClass("hit"))) {
+            $(temp).addClass("showShip")
+          }
+        }
+
+      }
+    })
+  console.log("game is starting")
+})
 
   // function to change td color to red if a ship is hit and dark blue if torpedoed but not hit
   //function also counts torpedoes used and turns click off
-  $("td").click(function(){
-    var currentTd = $(this).attr("id")
-    var dig0 = currentTd[0]
-    var dig1 = currentTd[1]
-    if (board[dig0][dig1] === ship){
-      $(this).addClass("hit");
-      hits ++
-      $("#hitTracker").text("Hits: " + hits)
-      if (hits === 5) {
-        $("#winLose").text("You Won!!!");
-        $("td").off("click");
-      }
-    } else {
-      $(this).addClass("torpedoed");
-    }
-    $(this).off("click");
-    torpedoesUsed++;
-    $("#torpedoTracker").text("Torpedoes used: " + torpedoesUsed);
-    if(torpedoesUsed === 25){
-      $("#winLose").text("Game Over.");
-      $("td").off("click");
-
-      for (i = 0; i < 5; i++){
-        var temp = "#" + shipArray[i]
-        if (!($(temp).hasClass("hit"))) {
-        $(temp).addClass("showShip")
-        }
-      }
-
-    }
-  })
 
 
 
